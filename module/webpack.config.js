83f15652b15c0c11ad511e19a1c175b8d6b3fd8e
nicklas <nicklasllanman@gmail.com>
2022-02-23 21:36:41 -0600

    added webpack to module

---
+++ module/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, './src/index.js'),
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+    publicPath: '/dist/'
+  },
+  optimization: {
+    usedExports: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
