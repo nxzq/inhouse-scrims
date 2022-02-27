eef6b89052a01dde70969dd01e1802d4a0a2d7d7
nicklas <nicklasllanman@gmail.com>
2022-02-26 20:46:17 -0600

    ui: footer text size

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -2,13 +2,13 @@ import githubIcon from '../img/github.svg'
 
 export default function Footer() {
   return (
-    <div className="flex flex-wrap w-full justify-center text-md p-2">
+    <div className="flex flex-wrap w-full justify-center text-sm p-2">
       <p className="text-slate-400 text-center mx-2">
         open source league of legends inhouse scrims matchmaking
       </p>
       <a href="https://github.com/exclamationpointhuman/inhouse-scrims/tree/main/module">
         <p className="text-slate-500 hover:text-slate-600 hover:underline mx-2 flex items-center justify-center">
-          <img src={githubIcon} alt="" className="w-5 h-5" />
+          <img src={githubIcon} alt="" className="w-4 h-4" />
           &nbsp;view algorithm here
         </p>
       </a>
