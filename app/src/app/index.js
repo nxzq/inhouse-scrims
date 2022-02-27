ec774dd1e9f0cbb15e429a107122526a27dfa22b
nicklas <nicklasllanman@gmail.com>
2022-02-26 21:47:03 -0600

    ui: added icons to all action buttons

--- app/src/app/index.js
+++ app/src/app/index.js
@@ -142,12 +142,27 @@ export default function App() {
                 </div>
               )}
               <div className="flex justify-center p-2 m-2">
-                <input
-                  type="submit"
-                  value="Generate Inhouse Lobbies"
-                  className="btn w-60"
-                />
-                <button onClick={(e) => handleClear(e)} className="btn w-24">
+                <button type="submit" className="btn items-center w-72">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                  </svg>
+                  Generate Inhouse Lobbies
+                </button>
+                <button
+                  onClick={(e) => handleClear(e)}
+                  className="btn items-center w-24"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5 mr-2"
--- app/src/components/Drafts.js
+++ app/src/components/Drafts.js
@@ -353,7 +353,21 @@ export default function Drafts({ players, toggleState, setErrors }) {
             Copy Current Lobby
           </button>
         </Tooltip>
-        <button onClick={() => toggleState()} className="btn w-56">
+        <button onClick={() => toggleState()} className="btn w-64">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"
+            />
+          </svg>
           Return to Player Selection
         </button>
       </div>
